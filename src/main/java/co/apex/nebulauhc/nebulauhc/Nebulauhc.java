05b7d388b564f4d16e607642b8c8aa88b2d07cb3
Louis Abery <60328026+aybery@users.noreply.github.com>
2022-07-23 22:17:48 +0100

    Initial commit

---
+++ src/main/java/co/apex/nebulauhc/nebulauhc/Nebulauhc.java
@@ -0,0 +1,17 @@
+package co.apex.nebulauhc.nebulauhc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nebulauhc extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
